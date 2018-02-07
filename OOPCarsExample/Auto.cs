84863abf6327f49ee790c40418a1714f3b340265
hmojica <hazael.mojica@peopleconnect.us>
2018-02-06 22:58:55 -0600

    primera sesion

---
+++ OOPCarsExample/Auto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace one {
+    public class Auto {
+        private string model;
+        public string Color;
+        public string Year { get; set; }
+        protected string bla;
+
+        public Auto() {
+            
+        }
+
+        public Auto(string model, string color, string year) {
+            this.model = model;
+            this.Color = color;
+            this.Year = year;
+        }
+        
+        public string Model {
+            get { return model;  }
+            set { model = value; }
+        }
+
+        public virtual void Arrancar() {
+            Console.WriteLine("Run run");
+        }
+
+        private class MiniAuto {
+            
+        }
+        
+    }
+}
---
+++ OOPCarsExample/AutoSport.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace one {
+    public class AutoSport : Auto {
+        public string PowerGears { get; set; }
+
+        public override void Arrancar() {
+            model = "";
+            Color = "";
+            bla = "";
+            Console.WriteLine("RRRuuunn RRRUUUNNNN");
+        }
+    }
+}
